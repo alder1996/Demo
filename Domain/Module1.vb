a3c4d1bbc48b008bd85c7faf4260fba052e6f15b
alder1996 <aldalink16@gmail.com>
2022-08-05 11:00:10 -0400

    Add files via upload

---
+++ Domain/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Net
+
+Module Module1
+
+    Sub Main()
+        Dim s As String = Dns.GetHostEntry(Dns.GetHostName()).AddressList _
+        .Where(Function(a As IPAddress) Not a.IsIPv6LinkLocal AndAlso Not a.IsIPv6Multicast AndAlso Not a.IsIPv6SiteLocal) _
+        .First() _
+        .ToString()
+        Console.Write(Dns.GetHostName() & vbCrLf)
+        Console.Write(s)
+        Console.ReadKey()
+        Console.Write(Dns.GetHostEntry(s).AddressList)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
